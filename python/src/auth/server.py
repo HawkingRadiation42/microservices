422bdc2bd809bb1527c1cee4f157588baa9075f3
HawkingRadiation42 <shrey.209303098@muj.manipal.edu>
2023-09-08 11:48:03 +0530

    added gitignore

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException, Header, Depends
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 from passlib.context import CryptContext
 import jwt
 import datetime
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -13,12 +16,12 @@ class User(BaseModel):
     username: str
     password: str
 
-def get_db():
+async def get_db():
     db_uri = os.environ.get("MONGODB_URI")
     db_name = os.environ.get("MONGODB_DB_NAME")
     if not (db_uri and db_name):
         raise HTTPException(status_code=500, detail="Database configuration missing")
-    client = MongoClient(db_uri)
+    client = AsyncIOMotorClient(db_uri)
     db = client[db_name]
     return db
 
@@ -26,9 +29,9 @@ def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
 @app.post("/login")
-def login(user: User):
-    db = get_db()
-    user_row = db.user.find_one({"email": user.username})
+async def login(user: User):
+    db = await get_db()
+    user_row = await db.user.find_one({"email": user.username})
 
     if user_row:
         email = user_row["email"]
@@ -39,10 +42,10 @@ def login(user: User):
         
         return create_jwt(user.username, os.environ.get("JWT_SECRET"), True)
     else:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(status_code=401, detail="No such user exists, please sign up.")
 
 @app.post("/validate")
-def validate(authorization: str = Header(None)):
+async def validate(authorization: str = Header(None)):
     if not authorization:
         raise HTTPException(status_code=401, detail="Missing credentials")
 
@@ -68,9 +71,9 @@ def create_jwt(username, secret, authz):
     )
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "Welcome to microservices auth home page!"}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
